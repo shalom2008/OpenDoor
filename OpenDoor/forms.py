d40dc36128d41c16c7ec534b7b70ec64c0a86574
shalom2008 <shalom2008@live.cn>
2017-10-27 17:34:41 +0800

    at tianjin

--- OpenDoor/forms.py
+++ OpenDoor/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, label='用户名', max_length=8)
-    password = forms.PasswordInput()
+    password = forms.CharField(required=True, label='密码', min_length=8, widget=forms.PasswordInput)
--- OpenDoor/views.py
+++ OpenDoor/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.generic import View
+
+from .forms import LoginForm
 
 
 class UserLoginView(View):
     def get(self, request):
-        return render(request, 'login.html')
+        login_form = LoginForm()
+        return render(request, 'login.html', {'form': login_form})
 
     def post(self, request):
         try:
-            username = request.POST.get('username', '')
-            password = request.POST.get('password', '')
-            user = User.objects.filter(username=username)
-            if not user:
-                raise Exception('该用户不存在！')
+            login_form = LoginForm(request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data.get('username', '')
+                password = login_form.cleaned_data.get('password', '')
+                user = User.objects.filter(username=username)
+                if not user:
+                    raise Exception('该用户不存在！')
 
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)
+                user = authenticate(request, username=username, password=password)
+                if user:
+                    login(request, user)
+                else:
+                    raise Exception('密码错误！')
+                return redirect('/')
             else:
-                raise Exception('密码错误！')
-            return redirect('/')
+                raise Exception('填写有误！')
+
         except Exception as e:
             error_message = str(e)
-            return render(request, 'login.html', {'error_message': error_message})
+            return render(request, 'login.html', {'error_message': error_message, 'form': login_form})
